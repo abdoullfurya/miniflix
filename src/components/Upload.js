65df39df4cbfce965fe6664ec2efe84682637f92
abdoullfurya <abdoulfurya@gmail.com>
2017-08-28 00:21:06 +0300

    mini netflix

---
+++ src/components/Upload.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+import Nav from './Nav';
+
+class Upload extends Component {
+
+  uploadWidget = () => {
+    window.cloudinary.openUploadWidget(
+
+    {
+      cloud_name: 'unicodeveloper',
+      upload_preset: 'b9ej8dr5',
+      tags: ['miniflix'],
+      sources: ['local', 'url', 'google_photos', 'facebook', 'image_search']
+    },
+    (error, result) => {
+      console.log("This is the result of the last uplaod", result);
+    });
+  }
+
+  render() {
+
+    return (
+      <div>
+        <Nav />
+        <h3 className="text-center">Upload Your 20-second Video in a Jiffy</h3>
+        <hr/>
+
+        <div className="col-sm-12">
+          <div className="jumbotron text-center">
+            <button onClick={this.uploadWidget} className="btn btn-lg btn-info"> Upload Video</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Upload;
